76446bab6576a6ffa4839ceb7539915d050a1aca
Kayke Galdiano <kaykegaldiano@gmail.com>
2023-06-29 17:53:47 -0300

    masking document, phone and birth date and checking if passwords are equal

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,2 +1,18 @@
-const name = document.querySelector('input[name="name"]')
-const email = document.querySelector('input[name="email"]')
+const form = document.querySelector('form')
+const documentInpt = document.querySelector('input[name="document"]')
+const phoneInpt = document.querySelector('input[name="phone"]')
+const birthDateInpt = document.querySelector('input[name="date"]')
+const passwordInpt = document.querySelector('input[name="password"]')
+const confirmPasswordInpt = document.querySelector('input[name="confirmPassword"]')
+
+VMasker(documentInpt).maskPattern('999.999.999-99')
+VMasker(phoneInpt).maskPattern('+99 99 99999-9999')
+VMasker(birthDateInpt).maskPattern('9999/99/99')
+
+form.addEventListener('submit', e => {
+    if (confirmPasswordInpt.value !== passwordInpt.value) {
+        e.preventDefault()
+        alert('Passwords must be equal!')
+        return
+    }
+})
